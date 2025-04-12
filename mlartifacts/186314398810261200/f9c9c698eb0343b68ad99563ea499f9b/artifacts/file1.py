ad1a9512dd126bff1b227625dadd06226d5e5223
gpreddy <gpreddy685@gmail.com>
2025-04-12 21:16:38 +0530

    mlflow and dagshub

---
+++ mlartifacts/186314398810261200/f9c9c698eb0343b68ad99563ea499f9b/artifacts/file1.py
@@ -0,0 +1,49 @@
+import mlflow
+import mlflow.sklearn
+from sklearn.datasets import load_wine
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix
+import matplotlib.pyplot as plt 
+import matplotlib
+matplotlib.use('Agg')
+import seaborn as sns
+mlflow.set_tracking_uri('http://localhost:5000')
+
+wine = load_wine()
+X = wine.data
+y = wine.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+max_depth = 10
+n_estimators = 10
+
+mlflow.set_experiment('Mlflow-1')
+
+with mlflow.start_run():
+    rf = RandomForestClassifier(max_depth=max_depth, n_estimators=n_estimators, random_state=42)
+    rf.fit(X_train, y_train)
+    y_pred = rf.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    cm = confusion_matrix(y_test, y_pred)   
+    plt.figure(figsize=(10,7))
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=wine)
+    plt.ylabel('Actual')
+    plt.xlabel('Predicted')
+    plt.title('Confusion Matrix')
+    plt.savefig("Cm-1.png")
+
+    mlflow.log_metric('accuracy', accuracy)
+    mlflow.log_param('max_depth', max_depth)
+    mlflow.log_param('n_estimators', n_estimators)
+    mlflow.log_param('test_size', 0.2)
+
+    mlflow.log_artifact("Cm-1.png")
+    mlflow.log_artifact(__file__)
+
+    mlflow.set_tags({"Author": "Priya", "Project": "Wine-Classification"})
+
+    mlflow.sklearn.load_model(rf, "Random Forest Model")
+    
+    print(f"Accuracy: {accuracy}")
